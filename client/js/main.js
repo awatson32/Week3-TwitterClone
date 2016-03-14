7b7becf5b782412f97a067dadf654259b5a29a14
awatson32 <audrey.watson32@gmail.com>
2016-03-14 11:54:37 -0500

    btn isn't updating list. >.<

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 /*Using document ready runs code only after the DOM is ready for js code to run more on that here: https://learn.jquery.com/using-jquery-core/document-ready */
 	function postData() {
+        $('.btn').on('click', function() {
+            var post = $('.status-box').val();
+            $('.posts').prepend('<li>' + post + '</li>')
+            // $('<li>').text(post).prependTo('.posts');
+        });
+        
 		/*This function should create a post request using jquery. When posted it should:
 			1) Add tweets to the 'database'
 			2) After posted prepend message to list of messages and clear input box */
+            
 	}
 
 	function getData() {
@@ -11,10 +18,10 @@ $(document).ready(function(){
 		/*This function should make a get request from 'database', parse the data and prepend each to the page*/
         $.get('/messages', function(data) {
             var tweet = data;
-            var text = tweet.split(' ');
+            var text = tweet.split('\n');
             for (var i in text) {
-                $('.posts').prepend('<li>' + text[i] + '<li>');
-                $('.posts').prepend('</br>');
+                $('.posts').append('<li>' + text[i] + '</li>');
+                $('.posts').append('</br>');
             }
         })
         // $.getJSON('/messages', function(data) {
@@ -25,7 +32,7 @@ $(document).ready(function(){
                      
         // })
 	}
-    console.log("this text works")
+
 	/*Calls function once page loaded to display tweets to page*/
 	getData();
 });
