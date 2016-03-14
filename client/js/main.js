bdf0bead3d1f2d57cf377fae139fa37e7dbf269c
awatson32 <audrey.watson32@gmail.com>
2016-03-14 17:17:26 -0500

    Will use .ajax instead of .get

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
 /*Using document ready runs code only after the DOM is ready for js code to run more on that here: https://learn.jquery.com/using-jquery-core/document-ready */
 	function postData() {
-        $('#send').on('click', function() {
-            var post = $('.status-box').val();
-            $('.posts').prepend('<li>' + post + '</li>');
-        });
+        
+    //     $.ajax({
+    //        type: 'POST',
+    //        url: '/messages',
+    //        beforeSend: function() {
+    //            $('#posts-container').append('<div id="load">Loading</div>');
+    //        },
+    //        complete: function() {
+    //            $('#load').remove();
+    //        },
+    //        success: function(data) {
+               
+    //        }
+    //    })
+       
+    //     $('#send').on('click', function() {
+    //         var post = $('.status-box').val();
+    //         $('.posts').prepend('<li>' + post + '</li>');
+    //     });
         
 		/*This function should create a post request using jquery. When posted it should:
 			1) Add tweets to the 'database'
@@ -13,16 +28,28 @@ $(document).ready(function(){
 	}
 
 	function getData() {
-           
-		/*This function should make a get request from 'database', parse the data and prepend each to the page*/
-        $.get('/messages', function(data) {
-            var tweet = data;
-            var text = tweet.split('\n');
-            for (var i in text) {
-                $('.posts').append('<li>' + text[i] + '</li>');
-                $('.posts').append('</br>');
+        /*This function should make a get request from 'database', parse the data and prepend each to the page*/
+        $.ajax({
+            url: '/messages',
+            method: 'GET',
+            context: $('.posts')
+        }).then(function(result) {
+            var tweet = result.split('\n');
+            for (var i in tweet) {
+                tweet[i] = JSON.parse(tweet[i]);
+                $('.posts').prepend('<li>' + '@' + tweet[i].userName + ': ' + tweet[i].text + '</li>');
             }
         });
+       
+        // $.get('/messages', function(data) {
+        //     var tweet = data;
+        //     var text = tweet.split('\n');
+        //     console.log(data, text);
+        //     for (var i in text) {
+        //         $('.posts').append('<li>' + text[i] + '</li>');
+        //         $('.posts').append('</br>');
+        //     }
+        // });
 	}
 
 	/*Calls function once page loaded to display tweets to page*/
